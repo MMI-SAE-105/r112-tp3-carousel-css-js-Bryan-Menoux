df4d9227b3d1de87a346acb469524898725760bc
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2024-12-14 18:04:56 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -4,16 +4,18 @@
 const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
+let premierItem = document.querySelector(".carousel__item");
+let scrollAmount = premierItem.clientWidth;
 
 // Largeur de défilement d’un item
 if (carousel) {
   // Scroll au clic sur le bouton précédent
   prevButton.addEventListener("click", () => {
-    carousel.scrollBy({ left: -260, behavior: "smooth" });
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
   });
 
   // Scroll au clic sur le bouton suivant
   nextButton.addEventListener("click", () => {
-    carousel.scrollBy({ left: +260, behavior: "smooth" });
+    carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
   });
 }
